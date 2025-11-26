5bb977c9dcf7b18d1631cc07c995998fb927c0c5
Crassous Hadrien <crassoushadrien@gmail.com>
2025-11-26 14:39:34 +0100

    edit ai2thor version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # Copyright 2022 Kaiyu Zheng
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,21 +18,23 @@
 from setuptools import setup
 from distutils.extension import Extension
 
-setup(name='cos-pomdp',
-      packages=['cospomdp', 'cospomdp_apps'],
-      version='0.1',
-      description='COS-POMDP',
-      python_requires='>3.6',
-      install_requires=[
-          'ai2thor==3.3.4',
-          'pomdp-py',
-          'tqdm',
-          'torch>=1.8.0',
-          'prettytable',
-          'pytz',
-          'pandas',
-          'seaborn',
-          'sciex==0.3',
-      ],
-      author='Kaiyu Zheng',
-      author_email='kaiyutony@gmail.com')
+setup(
+    name="cos-pomdp",
+    packages=["cospomdp", "cospomdp_apps"],
+    version="0.1",
+    description="COS-POMDP",
+    python_requires=">3.10",
+    install_requires=[
+        "ai2thor==5.0.0",
+        "pomdp-py",
+        "tqdm",
+        "torch>=1.8.0",
+        "prettytable",
+        "pytz",
+        "pandas",
+        "seaborn",
+        "sciex==0.3",
+    ],
+    author="Kaiyu Zheng",
+    author_email="kaiyutony@gmail.com",
+)
